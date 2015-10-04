14d99d068972c3cac05fdd52a0ba3361fe5f1300
Mark Donovan <mark@yieldex.com>
2015-10-04 01:23:32 -0600

    Events tied in.

--- app.js
+++ app.js
@@ -58,4 +58,109 @@ angular.module('beatspeak',
             src:'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/131613316' + paramsString.join('&')
         };
 
-    }]);
+    }])
+
+    .service('EventsService', ['Restangular',
+        function(Restangular) {
+            this.getEvents = function(callback) {
+
+                var _gapi = gapi || window['gapi'] || window.gapi;
+
+                //$scope.trustSrc = function(src) {
+                //    return $sce.trustAsResourceUrl(src);
+                //};
+
+                var APIKEY = 'AIzaSyDfK94EjiqyzwbZ1bku-dz4WkTVYnUDoKE';
+
+                function loadCalendarApi() {
+                    _gapi.client.setApiKey(APIKEY);
+                    _gapi.client.load('calendar', 'v3', listUpcomingEvents);
+                }
+
+                function listUpcomingEvents() {
+
+                    var request = _gapi.client.calendar.events.list({
+                        'calendarId': 's359u0ch0e55rv51c0i8vjbf04@group.calendar.google.com',
+                        'timeMin': (new Date()).toISOString(),
+                        'showDeleted': false,
+                        'singleEvents': true,
+                        'maxResults': 10,
+                        'orderBy': 'startTime'
+                    });
+
+                    request.execute(function(resp) {
+                        var events = resp.items;
+                        callback.apply(this, [events]);
+                    });
+                }
+
+                loadCalendarApi();
+
+            }
+        }
+    ])
+
+    .controller('EventsController', ['$scope', '$sce', 'EventsService', function($scope, $sce, EventsService) {
+
+        if( !$scope.events ) {
+            EventsService.getEvents(function(events){
+                // console.log(events);
+                $scope.events = [];
+
+                // clean things up
+                for(var i in events) {
+                    var event = events[i];
+                    // only show public events
+                    if(event.visibility == 'public') {
+
+                        var viewEvent = {
+                            summary:        event.summary,
+                            location:       event.location,
+                            startDateShort: moment(event.start.dateTime).format('DD/YY'),
+                            endDateShort:   moment(event.end.dateTime).format('DD/YY'),
+                            startDate:      moment(event.start.dateTime).format('MMMM Do YYYY, h:mm a'),
+                            endDate:        moment(event.end.dateTime).format('MMMM Do YYYY, h:mm a'),
+                            startTime:      moment(event.start.dateTime).format('h:mm a'),
+                            endTime:        moment(event.end.dateTime).format('h:mm a')
+                        };
+
+                        $scope.events.push(viewEvent);
+
+                    }
+                }
+
+                $scope.$apply();
+
+            });
+        }
+
+        //if (events.length > 0) {
+        //    for (i = 0; i < events.length; i++) {
+        //        var event = events[i];
+        //        ///console.log(event);
+        //
+        //        var when = event.start.dateTime;
+        //        if (!when) {
+        //            when = event.start.date;
+        //        }
+        //        appendPre(event.summary);
+        //        appendPre(new Date(when).toDateString());
+        //    }
+        //} else {
+        //    appendPre('No upcoming events found.');
+        //}
+        //function appendPre(message) {
+        //    var pre = document.getElementById('beatSpeakEvents');
+        //    var textContent = document.createTextNode(message);
+        //    pre.appendChild(textContent);
+        //    pre.appendChild(document.createElement('br'));
+        //}
+
+    }])
+
+
+
+
+
+
+;
